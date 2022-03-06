6e2c916e76d6ad5a7d9592570b28631b74e63866
Leo <leo@xuebin.me>
2022-03-06 17:39:53 +0800

    build: npm install @notionhq/client

---
+++ client/query.ts
@@ -0,0 +1,33 @@
+const { Client } = require('@notionhq/client')
+
+const notion = new Client({ auth: process.env.NOTION_API_KEY })
+
+;(async (): Promise<void> => {
+  const databaseId = '897e5a76-ae52-4b48-9fdf-e71f5945d1af'
+  const response = await notion.databases.query({
+    database_id: databaseId,
+    filter: {
+      or: [
+        {
+          property: 'In stock',
+          checkbox: {
+            equals: true,
+          },
+        },
+        {
+          property: 'Cost of next trip',
+          number: {
+            greater_than_or_equal_to: 2,
+          },
+        },
+      ],
+    },
+    sorts: [
+      {
+        property: 'Last ordered',
+        direction: 'ascending',
+      },
+    ],
+  })
+  console.info(`[LOG] -> response`, response)
+})()
